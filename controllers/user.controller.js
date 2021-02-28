ac6dccf36b1da0420a931fda6395beec1088827e
Ricardo Rodríguez Pérez <iscricardorodz@outlook.com>
2021-02-27 18:07:59 -0600

    Base de creasion de Rest server

---
+++ controllers/user.controller.js
@@ -0,0 +1,46 @@
+const { response } = require('express');
+
+
+const userGet = (req, res = response) => {
+    res.json({
+        msg: 'get API - controlador'
+    });
+}
+
+const userPut = (req, res = response) => {
+    res.json({
+        msg: 'put API - controlador'
+    });
+}
+
+const userPost = (req, res = response) => {
+
+    const { nombre, edad } = req.body;
+
+    res.json({
+        msg: 'post API - controlador',
+        nombre,
+        edad
+    });
+}
+
+const userDelete = (req, res = response) => {
+    res.json({
+        msg: 'delete API - controlador'
+    });
+}
+
+const userPatch = (req, res = response) => {
+    res.json({
+        msg: 'patch API - controlador'
+    });
+}
+
+
+module.exports = {
+    userGet,
+    userPut,
+    userPost,
+    userDelete,
+    userPatch
+}
---
+++ models/server.js
@@ -0,0 +1,44 @@
+require('dotenv').config( );
+
+const express = require('express')
+const cors = require('cors');
+
+class Server {
+
+    constructor() {
+        this.app = express();
+        this.port = process.env.PORT  || 3000;
+        this.usuariosPath = '/api/usuarios';
+
+        //Middleware
+        this.middlewares();
+
+        //Rutas de mi aplicación
+        this.routes();
+    }
+    
+    middlewares(){
+
+        //CORS
+        this.app.use( cors() );
+        //Lectura y parseo del body
+        this.app.use( express.json() );
+        //Directorio público
+        this.app.use( express.static('public'));
+
+    }
+
+
+    routes() {
+        
+        this.app.use( this.usuariosPath, require('../routes/user') );
+    }
+
+    listen() {
+        this.app.listen( this.port, () => {
+            console.log('Servidor corre en el puerto', this.port);
+        });
+    }
+}
+
+module.exports = Server;
